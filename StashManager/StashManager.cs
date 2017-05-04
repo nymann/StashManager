717cf47902737d0fa2fdaef009a8e654c56ecb02
Nymann <s144833@student.dtu.dk>
2017-05-04 10:02:04 +0200

    Test 2.

--- StashManager/StashManager.cs
+++ StashManager/StashManager.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System.Threading;
+using System.Windows.Forms;
+using PoeHUD.Controllers;
 using PoeHUD.Plugins;
+using SharpDX;
+using SharpDX.Direct3D9;
 using StashManager.Helper;
 
 namespace StashManager
@@ -13,13 +17,66 @@ namespace StashManager
             {
                 return;
             }
+            /*_inventoryZone = _ingameState.ReadObject<Element>(_ingameState.IngameUi.InventoryPanel.Address + Element.OffsetBuffers + 0x42C);
+            
+            foreach (var child in _inventoryZone.Children)
+            {
+                var item = child.AsObject<NormalInventoryItem>().Item;
+                if (string.IsNullOrEmpty(item?.Path))
+                {
+                    continue;
+                }
+
+                var position = child.GetClientRect();
+                var itemName = item.Path.Split('/').Last();
+                // Let's draw a frame arround each item in our inventory.
+                Graphics.DrawFrame(position, 2f, Color.Azure);
+                Graphics.DrawText(itemName, 20, new Vector2(position.X, position.Y), Color.DarkCyan);
+            }
+            */
 
             while (!IsAllStashTabsViewable())
             {
-                ViewAllTabs((int) stashPanel.TotalStashes);
+                ViewAllTabs((int)stashPanel.TotalStashes);
             }
 
-            ShowNameOfEachNonNulLTab();
+            //MessageBox.Show(GameController.Game.IngameState.ServerData.StashPanel.getStashName(0));
+            ShowItemCountOfEachNonNullTab();
+
+            // Kinda works.
+            /*var titleElement = stashPanel.getStashTitleElement(stashPanel.getStashName(0));
+            var position = titleElement.GetClientRect();
+            Graphics.DrawFrame(titleElement.GetClientRect(), 10f, Color.Aqua);
+            Graphics.DrawText(titleElement.Children.Count.ToString(), 20, new Vector2(position.X, position.Y));*/
+
+            /*for (var i = 0; i < stashPanel.TotalStashes; i++)
+            {
+                var titleElement = stashPanel.getStashTitleElement(stashPanel.getStashName(i));
+                var position = titleElement.GetClientRect();
+                Graphics.DrawText(stashPanel.getStashName(i), 20, new Vector2(position.X, position.Y));
+            }*/
+        }
+
+        #region tests
+        private void ShowItemCountOfEachNonNullTab()
+        {
+            var stashPanel = GameController.Game.IngameState.ServerData.StashPanel;
+
+            var numberOfStashes = (int)stashPanel.TotalStashes;
+            var content = "";
+            for (int i = 0; i < numberOfStashes; i++)
+            {
+                var stashTab = stashPanel.getStashInventory(i);
+                if (stashTab == null)
+                {
+                    content += "stash tab no. " + i + ", is null.\n";
+                    continue;
+                }
+                var itemCount = stashTab.ItemCount;
+                content += "stash tab no. " + i + ", has " + itemCount + " items in it.\n";
+            }
+
+            MessageBox.Show(content);
         }
 
         private void ShowNameOfEachNonNulLTab()
@@ -45,6 +102,9 @@ namespace StashManager
             MessageBox.Show(content);
         }
 
+
+        #endregion
+
         private bool IsAllStashTabsViewable()
         {
             var stashPanel = GameController.Game.IngameState.ServerData.StashPanel;
@@ -52,7 +112,7 @@ namespace StashManager
 
             for (var i = 0; i < stashPanel.TotalStashes; i++)
             {
-                if (stashPanel.getStashInventory(i) == null || stashPanel.getStashName(i) == null || stashPanel.getStashName(i).Equals(""))
+                if (stashPanel.getStashInventory(i) == null /*|| stashPanel.getStashName(i) == null*/)
                 {
                     continue;
                 }
@@ -65,14 +125,12 @@ namespace StashManager
 
         private static void ViewAllTabs(int ammountOfTabs)
         {
-            // View all the tabs, by simulating pressing on the Right Arrow key.
             for (var i = 0; i < ammountOfTabs; i++)
             {
                 Keyboard.keybd_event(VirtualKeys.Right, 0, KeyboardEventFlags.Keydown, 0);
                 Keyboard.keybd_event(VirtualKeys.Right, 0, KeyboardEventFlags.Keyup, 0);
             }
 
-            // Backtrack to the first tab, by simmulating Left Arrow key presses.
             for (var i = 0; i < ammountOfTabs; i++)
             {
                 Keyboard.keybd_event(VirtualKeys.Left, 0, KeyboardEventFlags.Keydown, 0);
